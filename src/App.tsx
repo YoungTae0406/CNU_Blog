bd810f15c0f2154fad858764c855b3a2502a2246
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-06-05 17:29:32 +0900

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import Home from './pages/Home';
 import Layout from './pages/Layout';
 import Post from './pages/Post';
 import Resume from './pages/Resume';
-import Write from './pages/Write';
 import Header from './components/Header';
+import Write from './pages/Write';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.DEV ? '/' : 'YoungTae0406/CNU_Blog/'}>
       <Routes>
         <Route element={<Header />}>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/resume" element={<Resume />} />
           </Route>
+          <Route path="/post/:postId" element={<Post />} />
           {/*todo (3-3) Post 추가*/}
         </Route>
+        <Route path="/write" element={<Write />} />
         {/*todo (5-1) Write 추가*/}
       </Routes>
     </Router>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'YoungTae0406/CNU_Blog/',
   plugins: [react()],
 });

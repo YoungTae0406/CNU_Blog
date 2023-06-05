d932c2f2ff36abffc80787ceed8483b182540995
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-06-05 19:51:31 +0900

    assignment

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,17 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <div style={{ fontSize: '18pt' }}>일리아칸 레이드 성공</div>
+      <div>2022 10</div>
+      <div style={{ fontSize: '18pt' }}>토익 860점 달성</div>
+      <div>2022 07</div>
+      <div style={{ fontSize: '18pt' }}>굶지마 요리 레시피 프로젝트</div>
+      <div>2022 02 ~ 2022 03</div>
+      <div>Django 프레임워크를 활용해 요리 재료와 결과물을 데이터베이스에 저장함.</div>
+      <div>웹페이지에서 재료 이미지를 클릭 시 조합해서 나오는 결과물을 이미지로 출력함.</div>
+      <div style={{ fontSize: '18pt' }}>2019 ANA 알고리즘 대회 3등</div>
+    </div>
+  );
 };
 
 export default Resume;

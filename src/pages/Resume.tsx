ce339d68c6e3941d28730076d1aa69e742de97c0
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-06-06 13:10:25 +0900

    final

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,10 +1,20 @@
 const Resume = () => {
   return (
     <div>
-      <div style={{ fontSize: '15pt' }}>
+      <div style={{ fontSize: '15pt', display: 'flex', alignItems: 'center' }}>
+        <img
+          src="https://i.namu.wiki/i/WC5Tu2-zqtYjlrCG6OQFGaHlm4Dxkw20kmzkZBjeESR5e_DSrpp9gJ0TXOj1J2y3k1xw42kuC9rDk50FRrAmr5RgvaTegDwi8bTii4N014YAKrc_LCAbJgjmNYhIDqV3BjVU1whOwAyrCnvgNqyDkg.webp"
+          alt="Iliakan Raid"
+          style={{
+            marginRight: '10px',
+            width: '50px',
+            height: '50px',
+          }}
+        />
         <h3>일리아칸 레이드 성공</h3>
       </div>
       <div>2022 10</div>
+      <div>친구 7명과 함께 장장 9시간에 걸쳐서 성공</div>
       <div style={{ fontSize: '15pt' }}>
         <h3>토익 860점 달성</h3>
       </div>
@@ -12,14 +22,12 @@ const Resume = () => {
       <div style={{ fontSize: '15pt' }}>
         <h3>굶지마 요리 레시피 프로젝트</h3>
       </div>
-      <br />
       <div>2022 02 ~ 2022 03</div>
       <div>Django 프레임워크를 활용해 요리 재료와 결과물을 데이터베이스에 저장함.</div>
       <div>웹페이지에서 재료 이미지를 클릭 시 조합해서 나오는 결과물을 이미지로 출력함.</div>
       <div style={{ fontSize: '15pt' }}>
         <h3>2019 ANA 알고리즘 대회 3등</h3>
       </div>
-      <br />
     </div>
   );
 };

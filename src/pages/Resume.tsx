1f16ca7ed234305b60208e68e7e8057fa3de83b5
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-06-06 11:42:01 +0900

    edit:size

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,25 +1,23 @@
 const Resume = () => {
   return (
     <div>
-      <div style={{ fontSize: '18pt' }}>
-        <h2>일리아칸 레이드 성공</h2>
+      <div style={{ fontSize: '15pt' }}>
+        <h3>일리아칸 레이드 성공</h3>
       </div>
-      <br />
       <div>2022 10</div>
-      <div style={{ fontSize: '18pt' }}>
-        <h2>토익 860점 달성</h2>
+      <div style={{ fontSize: '15pt' }}>
+        <h3>토익 860점 달성</h3>
       </div>
-      <br />
       <div>2022 07</div>
-      <div style={{ fontSize: '18pt' }}>
-        <h2>굶지마 요리 레시피 프로젝트</h2>
+      <div style={{ fontSize: '15pt' }}>
+        <h3>굶지마 요리 레시피 프로젝트</h3>
       </div>
       <br />
       <div>2022 02 ~ 2022 03</div>
       <div>Django 프레임워크를 활용해 요리 재료와 결과물을 데이터베이스에 저장함.</div>
       <div>웹페이지에서 재료 이미지를 클릭 시 조합해서 나오는 결과물을 이미지로 출력함.</div>
-      <div style={{ fontSize: '18pt' }}>
-        <h2>2019 ANA 알고리즘 대회 3등</h2>
+      <div style={{ fontSize: '15pt' }}>
+        <h3>2019 ANA 알고리즘 대회 3등</h3>
       </div>
       <br />
     </div>
